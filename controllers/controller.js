c4b5acdbbbaa18212409da9035058537642fe8da
predictably <predictably.user@gmail.com>
2018-07-27 21:38:53 +0530

    27-7-18

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,12 +3,12 @@ var app = angular.module('myapp' , []);
 
 app.controller('myCtlr' , function($scope) {
 
-	$scope.cartValue = 0 ;
+	 $scope.cartValue = 0 ;
 
 	$scope.increaseCartCount = function() {
 
-		$scope.cartValue = $scope.cartValue + 1;
+		$scope.cartValue += 1;
 
-		alert($scope.cartValue);
+		console.log($scope.cartValue);
 	}
 });
